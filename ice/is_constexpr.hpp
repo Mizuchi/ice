5bf7a0d5c1efcfc7801349f8587574bcd540552a
Tianjiao Yin <ytj000@gmail.com>
2015-02-06 15:03:46 -0800

    rewrite is_constexpr, now it is only support clang

--- ice/is_constexpr.hpp
+++ ice/is_constexpr.hpp
@@ -1,13 +1,6 @@
-#include<type_traits>
-namespace ice {
-namespace detail {
-template <typename T> constexpr int makeprval(T) {
-    return 0;
-}
-} // namespace detail
-} // namespace yuki
-
 #define IS_CONSTEXPR(expr)                                                     \
-    (__builtin_constant_p(expr) ||                                             \
-     __builtin_constant_p(::ice::detail::makeprval(expr)) ||                  \
-     noexcept(::ice::detail::makeprval(expr)))
+    (__builtin_constant_p(expr) || __builtin_constant_p(({                     \
+        auto _ = (expr);                                                       \
+        static_cast<void>(_);                                                  \
+        0;                                                                     \
+                                   })))
