1b2c7f9605c221d7849d57cdad811e927eeaf856
Tianjiao Yin <ytj000@gmail.com>
2015-02-16 00:57:11 -0800

    add benchmark

---
+++ benchmark/a.cpp
@@ -0,0 +1,59 @@
+#include "../ice/ice.hpp"
+#include <chrono>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+const int maxDimension = 20;
+
+struct Model {
+    int dimension;
+    double w[maxDimension];
+    double b;
+};
+
+template <class T>
+bool isFirstClass(folly::ice::Any<T> t, std::vector<double> x) {
+    double s = 0;
+    for (int i = 0; i < t.get().dimension; i++) {
+        s += t.get().w[i] * x[i];
+    }
+    return s - t.get().b >= 0;
+}
+
+std::vector<std::vector<double>> gen_examples(int dimension, int count) {
+    auto gen_random = []() {
+        double r = double(rand()) / RAND_MAX;
+        return r * 10 - 5; // return random number in [-5, 5]
+    };
+
+    auto gen_example = [=]() {
+        std::vector<double> x(dimension);
+        std::generate(x.begin(), x.end(), gen_random);
+        return x;
+    };
+
+    std::vector<std::vector<double>> ret(count);
+    std::generate(ret.begin(), ret.end(), gen_example);
+    return ret;
+}
+
+constexpr Model get_model() {
+    return {10, {-0.9, 4.1, -3.5, -4.8, -3.1, 0.4, 1.6, -1.2, 4.9, 4.5}, 0};
+}
+
+int main() {
+    auto examples = gen_examples(10, 5e6);
+    bool b = false;
+    auto m = ICE(get_model());
+
+    auto t1 = std::chrono::system_clock::now();
+    for (auto &i : examples) {
+        b ^= isFirstClass(m, i);
+    }
+    auto t2 = std::chrono::system_clock::now();
+    std::cout << std::chrono::duration<double>(t2 - t1).count() << " seconds"
+              << std::endl;
+
+    return b;
+}
